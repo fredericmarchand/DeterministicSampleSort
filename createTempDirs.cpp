ced1909c19f5a65248a721a43e3e6cb568b05876
fredericmarchand <fredericmarchand@cmail.carleton.ca>
2014-12-05 23:54:13 -0500

    Added MPI number generator

--- createTempDirs.cpp
+++ createTempDirs.cpp
@@ -8,26 +8,66 @@
 
 using namespace std;
 
+#define MASK 0777
+
 #include "mpi.h"
 
 int main(int argc, char *argv[])
 {
-    char path[256];
-    ifstream input;
-    string buffer;
+	char path[256];
+	char filename[256];
+	char permissions[256];
 
-    int p;
-    int id;
+	int procs;
+	int id;
 
-    MPI::Init(argc, argv); //  Initialize MPI.
-    p = MPI::COMM_WORLD.Get_size(); //  Get the number of processes.
-    id = MPI::COMM_WORLD.Get_rank(); //  Get the individual process ID.
+	int n, p;
 
-    sprintf(path, "/tmp/fredericmarchand");
-    mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
-    
-    // Terminate MPI.
-    MPI::Finalize();
+	MPI::Init(argc, argv); //  Initialize MPI
 
-    return 0;
+	if (argc != 3)
+	{
+		MPI::Finalize();		
+		return 1;
+	}
+
+	procs = MPI::COMM_WORLD.Get_size(); //  Get the number of processes.
+	id = MPI::COMM_WORLD.Get_rank(); //  Get the individual process ID.
+
+	n = atoi(argv[1]);
+	p = atoi(argv[2]);
+
+	sprintf(path, "/tmp/fredericmarchand");
+	mkdir(path, MASK); 
+
+	sprintf(filename, "%s/input-%d.txt", path, id);
+
+	//Write to file
+	ofstream outputFile (filename);
+	
+	if (!outputFile.is_open())
+	{
+		cerr << "Could not open file " << filename << endl;
+		return 1;   /// Error
+	}
+	
+	outputFile << n << endl;
+	outputFile << p << endl;
+
+	for (int i = 0; i < n/p; ++i)
+	{
+		outputFile << rand() % 100000 << endl;
+	}
+	
+	outputFile.close();
+
+	sprintf(permissions, "chmod 777 %s", path);
+	system(permissions);
+	sprintf(permissions, "chmod 777 %s", filename);
+	system(permissions);
+
+	// Terminate MPI.
+	MPI::Finalize();
+
+	return 0;
 }
